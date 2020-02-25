0d4d85b4b9adbf74d0e34ed25450a911d32f39f3
Norman Dube <ndubewebdesigner@gmail.com>
2020-02-25 11:11:47 +0200

    Cleaning up

--- server/app.js
+++ server/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
 const mongoose = require('mongoose');
-const postsRoutes = require('./routes/posts-routes');
 const moviesRoutes = require('./routes/movies-routes');
 
 
 const app = express();
-// mongodb+srv://normy:saber24teeth@mean-gxsi7.mongodb.net/mean-stack?retryWrites=true&w=majority
 mongoose.connect(
     'mongodb+srv://ndube1989:saber24teeth@movies-icwdd.mongodb.net/test?retryWrites=true&w=majority/movies', {
         useNewUrlParser: true,
@@ -38,7 +36,7 @@ app.use((req, res, next) => {
 app.get('', (req, res, next) => {
     res.send('<h1 style="text-align: center">Welcome to the Movies API</h1>');
 })
-app.use('/api/posts', postsRoutes);
+
 app.use('/api/movies', moviesRoutes);
 
 module.exports = app;
